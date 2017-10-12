f75f98485dff07400bd87e4f0afeb6e60e6306fc
Andrés <aabreu>
2017-10-12 14:42:12 -0400

    listView

--- index.android.js
+++ index.android.js
@@ -4,36 +4,39 @@ import {
     Text,
     View,
     StatusBar,
-    FlatList,
-    SectionList
+    ListView
 } from 'react-native';
 
 export default class  SectionListApp extends Component{
     constructor(props){
         super(props)
+ 
     }
     render(){
-        var data = [
-            {title: "A", data: ["Alexander", "Alan Turing"], key: "A"},
-            {title: "B", data: ["Barbara", "Brooke", "Brenda"], key: "B"},
-            {title: "C", data: ["David", "Dylan", "Diego", "Dakota"], key: "D"},
+
+        var standarDataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        var movies = [
+            {
+                title: "Star Wars",
+                releaseYear: "1977"
+            },
+            {
+                title: "The Matrix",
+                releaseYear: "1999"
+            }
         ];
+        var clonedMovies = standarDataSource.cloneWithRows(movies);
         return(
             <View style= {{ flex:1}}>
                 <StatusBar hidden={true}/>
-
-                <SectionList
-                    sections = {data}
-                    renderItem = {({item}) => 
-                    <Text 
-                    style = {{ marginBottom: 10, fontSize: 40, height: 50, backgroundColor: 'lightblue'}}
-                    > {item} </Text>}
-                    renderSectionHeader={
-                        ({section}) => <Text>{section.title}</Text>
-
+                <ListView
+                    dataSource={clonedMovies}
+                    renderRow={
+                        (rowData) => <Text> Title: {rowData.title}, release year: {rowData.releaseYear} </Text>
                     }
                 >
-                </SectionList>
+
+                </ListView>
             </View>
         );
     }
